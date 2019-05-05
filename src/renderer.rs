95fb4d363f0976631deca7a5ebbec1e7cfc527c0
evolix1 <evolix1@gmail.com>
2019-05-05 19:55:39 +0200

    feat(renderer): Render board in a given rect

--- src/renderer.rs
+++ src/renderer.rs
@@ -12,18 +12,44 @@ pub fn render(
 ) -> Result<(), String> {
     canvas.set_draw_color(Color::RGB(255, 0, 0));
     canvas.clear();
-
-    let (_width, _height) = canvas.output_size()?;
-
-    let tile_geom = Rect::new(0, 0, 32, 32);
-    for y in 0i32..board.rows as i32 {
-        for x in 0i32..board.columns as i32 {
-            let tile_screen = Rect::new(x * 32, y * 32, 32, 32);
-            canvas.copy(&textures[0], tile_geom, tile_screen)?;
-        }
-    }
+    
+    let (width, height) = canvas.output_size()?;
+    let board_screen_rect = Rect::new(10, 10, width - 20, height - 20);
+    render_board(canvas, &textures[0], board, &board_screen_rect)?;
 
     canvas.present();
+
+    Ok(())
+}
+
+
+fn render_board(
+    canvas: &mut WindowCanvas,
+    texture: &Texture,
+    board: &Board,
+    rect: &Rect
+) -> Result<(), String> {
+    let width = rect.width() as f32;
+    let height = rect.height() as f32;
+
+    let tile_geom = Rect::new(0, 0, 32, 32);
+    
+    for y in 0..board.rows {
+        for x in 0..board.columns {
+            let px = ((x as f32 / board.columns as f32) * width).floor();
+            let py = ((y as f32 / board.rows as f32) * height).floor();
+            
+            let next_x = (((x as f32 + 1f32) / board.columns as f32) * width).floor();
+            let next_y = (((y as f32 + 1f32) / board.rows as f32) * height).floor();
+            
+            let tile_screen = Rect::new(
+                rect.x() + px as i32, 
+                rect.y() + py as i32, 
+                (next_x - px) as u32, 
+                (next_y - py) as u32);
+            canvas.copy(&texture, tile_geom, tile_screen)?;
+        }
+    }
 
     Ok(())
 }
