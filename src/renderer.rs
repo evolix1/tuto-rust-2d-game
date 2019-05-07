1b63290ffe4e39e25a3ba7b87da77163058d912e
evolix1 <evolix1@gmail.com>
2019-05-07 18:33:49 +0200

    feat(render): Add sprite and aspect ratio
    * Add sprite management (along side texture)
    * Add aspect ratio when copy sprite onto canvas
    * Improve internal workflow of renderer by spliting into functions with clearer roles

--- src/renderer.rs
+++ src/renderer.rs
@@ -3,139 +3,182 @@ use sdl2::pixels::Color;
 use sdl2::render::{Canvas, Texture};
 
 use crate::board::Board;
-use crate::texture::DrawContext;
+use crate::texture::{DrawContext, SpriteId};
+
+
+#[allow(dead_code)]
+pub enum AspectRatio {
+    /// Dimensions are scaled to fit the container
+    Stretch,    
+    /// Preserve ratio, and make it fit inside the container
+    KeepIn,     
+}
 
 
 pub struct Renderer<'a> {
     background_color: Color,
-    context: DrawContext<'a>,
-    board_texture: Option<(usize, u32, u32)>
+    draw_ctx: DrawContext<'a>,
 }
 
 
 impl<'a> Renderer<'a> {
-    pub fn new(context: DrawContext<'a>) -> Renderer<'a> {
+    pub fn new(draw_ctx: DrawContext<'a>) -> Renderer<'a> {
         Renderer {
             background_color: Color::RGB(220, 10, 10),
-            context,
-            board_texture: None
+            draw_ctx,
         }
     }
 
+    
     pub fn render(&mut self, board: &Board) -> Result<(), String> 
     {
-        self.prepare()?;
+        self.prepare(board)?;
 
-        self.render_board(board)?;
+        self.render_all()?;
 
-        self.context.canvas.present();
-
-        Ok(())
-    }
-
-    pub fn prepare(&mut self) -> Result<(), String> {
-        self.context.canvas.set_draw_color(self.background_color);
-        self.context.canvas.clear();
-        
-        Ok(())
-    }
-
-    pub fn render_board(
-        &mut self,
-        board: &Board,
-        ) -> Result<(), String> 
-    {
-        let (width, height) = self.context.canvas.output_size()?;
-        let board_screen_rect = Rect::new(10, 10, width - 20, height - 20);
-        
-        // If first time - we don't have generated the picture
-        if self.board_texture.is_none() {
-            println!("Generating board background...");
-            
-            // Create the texture
-            let format = self.context.textures[0].query().format;
-            let width = 32 * board.columns as u32;
-            let height = 32 * board.rows as u32;
-            
-            let mut board_texture = match self.context.creator.create_texture_target(
-                format,
-                width,
-                height) {
-                Err(err) => return Err(format!("{:?}", err)),
-                Ok(v) => v
-            };
-
-            // Render the texture
-            let canvas = &mut self.context.canvas;
-            let tile_texture = &self.context.textures[0];
-            
-            match canvas.with_texture_canvas(
-                &mut board_texture,
-                |texture_canvas| {
-                    // TODO how to propagate error 
-                    // (because here we cannot return something)
-                    let _r = Self::paint_board(
-                        texture_canvas, 
-                        &tile_texture,
-                        board);
-                }) {
-                Err(err) => return Err(format!("{:?}", err)),
-                _ => ()
-            }
-            
-            let texture_idx = self.context.add_texture(board_texture);
-            self.board_texture = Some((texture_idx, width, height));
-        }
-        
-        if let Some((id, width, height)) = self.board_texture {
-            let texture = &self.context.textures[id];
-            let geom = Rect::new(0, 0, width, height);
-            
-            self.context.canvas.copy(
-                texture,
-                geom, 
-                board_screen_rect)?;
-        }
+        self.draw_ctx.canvas.present();
 
         Ok(())
     }
 
     
-    fn paint_board<T>(
+    pub fn prepare(&mut self, board: &Board) -> Result<(), String> {
+        self.draw_ctx.canvas.set_draw_color(self.background_color);
+        self.draw_ctx.canvas.clear();
+
+        if !self.draw_ctx.sprites.contains_key(&SpriteId::DefaultBoard) {
+            self.init_board(board)?;
+        }
+
+        Ok(())
+    }
+    
+    
+    /**
+     * Render all game items.
+     */
+    pub fn render_all(&mut self) -> Result<(), String> 
+    {
+        let (width, height) = self.draw_ctx.canvas.output_size()?;
+        let geom = Rect::new(10, 10, width - 20, height - 20);
+
+        //let paint_rect = Self::keepAspectRatio(screen_rect, sprite.geom);
+        self.paint_sprite(&SpriteId::DefaultBoard, geom, AspectRatio::KeepIn)?;
+
+        Ok(())
+    }
+
+
+    // Private API below
+    fn paint_sprite(
+        &mut self, 
+        id: &SpriteId, 
+        area: Rect,
+        aspect: AspectRatio
+        ) -> Result<(), String> {
+        let sprite = self.draw_ctx.sprites.get(id)
+            .ok_or_else(|| format!("missing sprite"))?;
+        
+        let texture = &self.draw_ctx.textures[sprite.texture_id];
+        
+        let display_geom = match aspect {
+            AspectRatio::Stretch => area,
+            AspectRatio::KeepIn => {
+                let width = (area.height() as f32 
+                             * sprite.geom.width() as f32 
+                             / sprite.geom.height() as f32).floor() as u32;
+                let height = (area.width() as f32 
+                              * sprite.geom.height() as f32 
+                              / sprite.geom.width() as f32).floor() as u32;
+                
+                if width < area.width() {
+                    Rect::from_center(area.center(), width, area.height())
+                } else {
+                    Rect::from_center(area.center(), area.width(), height)
+                }
+            },
+        };
+        
+        self.draw_ctx.canvas.copy(texture, sprite.geom, display_geom)?;
+        Ok(())
+    }
+
+
+    fn init_board(&mut self, board: &Board) -> Result<(), String> {
+        let board_cell = self.draw_ctx.sprites
+            .get(&SpriteId::BoardCell)
+            .expect("board cell sprite exists");
+
+        let format = self.draw_ctx.textures[board_cell.texture_id].query().format;
+        let width = board_cell.geom.width() * board.columns as u32;
+        let height = board_cell.geom.height() * board.rows as u32;
+        
+        // Create the texture
+        let mut board_texture = self.draw_ctx.creator
+            .create_texture_target(format, width, height)
+            .map_err(|err| format!("{:?}", err))?;
+
+        // Render the texture
+        let canvas = &mut self.draw_ctx.canvas;
+        let tile_texture = &self.draw_ctx.textures[board_cell.texture_id];
+
+        let mut draw_err = Ok(());
+        canvas.with_texture_canvas(
+            &mut board_texture,
+            |texture_canvas| { 
+                draw_err = Self::draw_board(texture_canvas, &tile_texture, board);
+            })
+            .map_err(|err| format!("{:?}", err))?;
+        // re-raise error of draw
+        draw_err?;
+
+        // save texture
+        let board = self.draw_ctx.add_sprite_from_texture(
+            board_texture, 
+            SpriteId::SizedBoard{ width, height });
+        
+        // Remember this sprite for being the default board sprite
+        self.draw_ctx.sprites.insert(SpriteId::DefaultBoard, board);
+
+        Ok(())
+    }
+
+
+    fn draw_board<T>(
         canvas: &mut Canvas<T>,
         texture: &Texture,
         board: &Board,
         ) -> Result<(), String> 
         where T: sdl2::render::RenderTarget
-    {
-        let (width, height) = canvas.output_size()?;
-        let width = width as f32;
-        let height = height as f32;
+        {
+            let (width, height) = canvas.output_size()?;
+            let width = width as f32;
+            let height = height as f32;
 
-        let tile_geom = Rect::new(0, 0, 32, 32);
+            let tile_geom = Rect::new(0, 0, 32, 32);
 
-        for y in 0..board.rows {
-            for x in 0..board.columns {
-                let px = ((x as f32 / board.columns as f32) * width).floor();
-                let py = ((y as f32 / board.rows as f32) * height).floor();
+            for y in 0..board.rows {
+                for x in 0..board.columns {
+                    let px = ((x as f32 / board.columns as f32) * width).floor();
+                    let py = ((y as f32 / board.rows as f32) * height).floor();
 
-                let next_x = (((x as f32 + 1f32) / board.columns as f32) * width).floor();
-                let next_y = (((y as f32 + 1f32) / board.rows as f32) * height).floor();
+                    let next_x = (((x as f32 + 1f32) / board.columns as f32) * width).floor();
+                    let next_y = (((y as f32 + 1f32) / board.rows as f32) * height).floor();
 
-                let tile_screen = Rect::new(
-                    px as i32, 
-                    py as i32, 
-                    (next_x - px) as u32, 
-                    (next_y - py) as u32);
-                
-                canvas.copy(
-                    texture, 
-                    tile_geom, 
-                    tile_screen)?;
+                    let tile_screen = Rect::new(
+                        px as i32, 
+                        py as i32, 
+                        (next_x - px) as u32, 
+                        (next_y - py) as u32);
+
+                    canvas.copy(
+                        texture, 
+                        tile_geom, 
+                        tile_screen)?;
+                }
             }
-        }
 
-        Ok(())
-    }
+            Ok(())
+        }
 
 }
--- src/texture.rs
+++ src/texture.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+use sdl2::rect::{Rect};
 use sdl2::render::{Texture, Canvas, TextureCreator};
 use sdl2::video::{Window, WindowContext};
 use sdl2::image::{LoadTexture};
@@ -7,6 +10,7 @@ pub struct DrawContext<'a> {
     pub canvas: &'a mut Canvas<Window>,
     pub creator: &'a TextureCreator<WindowContext>,
     pub textures: Vec<Texture<'a>>,
+    pub sprites: HashMap<SpriteId, Sprite>,
 }
 
 
@@ -18,7 +22,8 @@ impl<'a> DrawContext<'a> {
         DrawContext {
             canvas,
             creator,
-            textures: vec![]
+            textures: Vec::new(),
+            sprites: HashMap::new(),
         }
     }
 
@@ -26,11 +31,42 @@ impl<'a> DrawContext<'a> {
         self.textures = vec![
             self.creator.load_texture("assets/all.png")?,
         ];
+        
+        self.sprites.insert(
+            SpriteId::BoardCell,
+            Sprite { texture_id: 0, geom: Rect::new(0, 0, 32, 32) });
+        
         Ok(())
+    }
+
+    pub fn add_sprite_from_texture(&mut self, texture: Texture<'a>, id: SpriteId) -> Sprite {
+        let info = texture.query();
+        let geom = Rect::new(0, 0, info.width, info.height);
+        
+        let texture_id = self.add_texture(texture);
+        
+        let sprite = Sprite { texture_id, geom };
+        self.sprites.insert(id, sprite.clone());
+        
+        sprite
     }
 
     pub fn add_texture(&mut self, texture: Texture<'a>) -> usize {
         self.textures.push(texture);
         self.textures.len() - 1
     }
+}
+
+
+#[derive(PartialEq, Eq, Hash)]
+pub enum SpriteId {
+    BoardCell,
+    SizedBoard { width: u32, height: u32 },
+    DefaultBoard,
+}
+
+#[derive(Clone)]
+pub struct Sprite {
+    pub texture_id: usize,
+    pub geom: Rect
 }
